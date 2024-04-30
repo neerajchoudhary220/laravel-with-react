43ff993a9c2983a27cf22f05d7aac75ec835440f
--expiry-date <neeraj@pairroxz.in>
2024-04-30 11:18:36 +0530

    updated

---
+++ resources/js/app.jsx
@@ -0,0 +1,10 @@
+import './bootstrap';
+import './bootstrap';
+import '../css/app.css'
+
+import ReactDOM from 'react-dom/client';        
+import Home from './components/Home';
+
+ReactDOM.createRoot(document.getElementById('app')).render(     
+    <Home />        
+);
